27730ef5a18e21103b2a373aeed8cdfb3ae2b101
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-02-28 12:14:44 -0500

    add loader

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-// const apiUrl = 'http://localhost:8000'
-const apiUrl = 'https://vietidols-server.herokuapp.com'
-export default apiUrl
+// const apiUrl = "http://localhost:8000";
+const apiUrl = "https://vietidols-server.herokuapp.com";
+export default apiUrl;
